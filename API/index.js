3342367277a040be9b201e00a5317c923cee50ea
Rudá Mafra <122709872+Ruda-Mafra@users.noreply.github.com>
2024-09-12 00:55:36 -0300

    DB connected and running

--- API/index.js
+++ API/index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
+// connect DB
+mongoose
+  .connect(process.env.MONGOOSEDB_URL)
+  .then(() => console.log("db connected"))
+  .then((err) => {
+    err;
+  });
 
-
-app.get("/", (req,res)=> {
-  res.send("app is f running")
-})
-
-
+app.get("/", (req, res) => {
+  res.send("app is f running");
+});
 
 app.listen(PORT || 9000, () => {
   console.log(`server is running on ${PORT}`);
 });
+
+// rudamafra
+// J0aquina
+
+// mongodb+srv://rudamafra:J0aquina@cluster0.doy06.mongodb.net/
